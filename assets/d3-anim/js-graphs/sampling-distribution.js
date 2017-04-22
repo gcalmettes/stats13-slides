9c44cc2dc086dd6039cbe29b1f4f7d3cf63024bc
gcalmettes <guillaume.calmettes@gmail.com>
2017-04-22 16:54:27 -0700

    removed initial observation line in sampling distribution d3 graph

--- assets/d3-anim/js-graphs/sampling-distribution.js
+++ assets/d3-anim/js-graphs/sampling-distribution.js
@@ -6,7 +6,7 @@ gc.samplingdist = gc.samplingdist || {};
 
 // Set the dimensions of the canvas / graph
 gc.samplingdist.margin = {top: 15, right: 30, bottom: 100, left: 30};
-    
+
 gc.samplingdist.width = 850 - gc.samplingdist.margin.left - gc.samplingdist.margin.right;
 gc.samplingdist.height = 450 - gc.samplingdist.margin.top - gc.samplingdist.margin.bottom;
 
@@ -22,7 +22,7 @@ gc.samplingdist.nsample = 100
 gc.samplingdist.svg = d3.select("#samplingdist .placeholder").append("svg")
     .attr("width", gc.samplingdist.width + gc.samplingdist.margin.left + gc.samplingdist.margin.right)
     .attr("height", gc.samplingdist.height + gc.samplingdist.margin.top + gc.samplingdist.margin.bottom);
-// histogram   
+// histogram
 gc.samplingdist.hist_plot = gc.samplingdist.svg.append("g")
     .attr("transform", "translate(" + gc.samplingdist.margin.left + "," + gc.samplingdist.margin.top + ")");
 
@@ -36,9 +36,9 @@ gc.samplingdist.hist_plot.append("g")
     .attr("class", "axis axis--x")
     .attr("transform", "translate(0," + gc.samplingdist.height + ")")
     .call(d3.axisBottom(gc.samplingdist.xscale));
-gc.samplingdist.hist_plot.append("text")             
+gc.samplingdist.hist_plot.append("text")
   .attr("transform",
-        "translate(" + (gc.samplingdist.width/2) + " ," + 
+        "translate(" + (gc.samplingdist.width/2) + " ," +
                        (gc.samplingdist.height + gc.samplingdist.margin.top + 50) + ")")
   .style("text-anchor", "middle")
   .style("fill", "#d2d2d2")
@@ -81,21 +81,21 @@ gc.samplingdist.update = function() {
     .thresholds(gc.samplingdist.xscale.ticks(Math.min(100, gc.samplingdist.samplesize)))
     // .thresholds(d3.thresholdFreedmanDiaconis(gc.samplingdist.data, d3.min(gc.samplingdist.data), d3.max(gc.samplingdist.data)))
     (gc.samplingdist.data);
- 
+
   // gc.samplingdist.bins = gc.samplingdist.bins.filter(function (d) {return d.length>0})
 
   gc.samplingdist.hist_plot.selectAll(".bar").remove();
   gc.samplingdist.hist_plot.selectAll(".line").remove();
-  
-  // Set y-scale based on data. 
+
+  // Set y-scale based on data.
   gc.samplingdist.yscale.domain([0, d3.max(gc.samplingdist.bins, d => d.length)]);
-  
+
   gc.samplingdist.bar = gc.samplingdist.hist_plot.selectAll(".bar")
     .data(gc.samplingdist.bins)
     .enter().append("g")
       .attr("class", "bar")
       .attr("transform", function(d) { return "translate(" + gc.samplingdist.xscale(d.x0) + "," + gc.samplingdist.yscale(d.length) + ")"; });
-  
+
   gc.samplingdist.bar.append("rect")
     .attr("fill", "steelblue")
     .attr("x", 1)
@@ -113,15 +113,16 @@ gc.samplingdist.update = function() {
       .style("stroke", "#d9534f")
       .style("fill", "none");
 
-  gc.samplingdist.svg.append("svg:line")
-        .attr("class", "line")
-        .attr("x1", 0.875*gc.samplingdist.width)
-        .attr("y1", gc.samplingdist.height+gc.samplingdist.margin.top)
-        .attr("x2", 0.875*gc.samplingdist.width)
-        .attr("y2", 0)
-        .style("stroke-width", 4)
-        .style("stroke", "#f0ad4e")
-        .style("fill", "none");
+  //if want a line to show initial observation
+  // gc.samplingdist.svg.append("svg:line")
+  //       .attr("class", "line")
+  //       .attr("x1", 0.875*gc.samplingdist.width)
+  //       .attr("y1", gc.samplingdist.height+gc.samplingdist.margin.top)
+  //       .attr("x2", 0.875*gc.samplingdist.width)
+  //       .attr("y2", 0)
+  //       .style("stroke-width", 4)
+  //       .style("stroke", "#f0ad4e")
+  //       .style("fill", "none");
 
 };
 
@@ -155,4 +156,3 @@ d3.select("#samplingdist #nsample")
     d3.select("#samplingdist #nsample-value").text(+this.value);
     gc.samplingdist.update();
 });
-
